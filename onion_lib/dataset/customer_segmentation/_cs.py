b747e96533881e71237c4c1d0f2671482afcc8f4
kid2201 <khanhsly22@gmail.com>
2021-08-16 18:10:49 +0700

    add statistics data

--- onion_lib/dataset/customer_segmentation/_cs.py
+++ onion_lib/dataset/customer_segmentation/_cs.py
@@ -1,6 +1,7 @@
 import pathlib
-import os 
+import os
+
 
 def get_dataset_path(data_name):
     current_directory = pathlib.Path(__file__).parent.resolve()
-    return os.path.join(current_directory,data_name) 
+    return os.path.join(current_directory, data_name)
